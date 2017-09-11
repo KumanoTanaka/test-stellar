77c3c09d12df89e5334a720bbafc1d853ad5b711
KumanoTanaka <hiroki.tanaka@kumano-te.com>
2017-09-11 23:23:38 +0900

    refs #5 add callbacks & stellar.toml

--- callbacks/index.js
+++ callbacks/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var account = require('./account');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/receive', function (request, response) {
   var payment = request.body;
@@ -12,7 +14,53 @@ app.get('/receive', function (request, response) {
   response.status(200).end();
 });
 
-// TODO add compliance server callback here.
+app.post('/compliance/fetch_info', function (request, response) {
+   var addressParts = request.body.address.split('*');
+   var friendlyId = addressParts[0];
+   account.findByFriendlyId(friendlyId).then(function(account) {
+     response.json({
+       first_name: account.first_name,
+       last_name: account.last_name,
+       address: 'GATLCZMS3ITTVSCM2VB55KT2I7OHHXOWD2QM6O4BNK5LQTD2A3T7DINC'
+     });
+     response.end();
+   }).catch(function(error) {
+     console.log('Fetch Info Error:', error);
+     response.status(500).end(error.message);
+   });
+});
+
+app.post('/compliance/sanctions', function (request, response) {
+  var sender = request.body.sender;
+  switch (sender) {
+    case 'jack_brown*stellar-kumano-te.com':
+      response.status(403).end();
+      break;
+    case 'steintor_jakupsson*stellar-kumano-te.com':
+      console.log('need human help!');
+      response.status(202).json({pending: 3600}).end();
+      break;
+    default:
+      response.status(200).end();
+      break;
+  }
+});
+
+app.post('/compliance/ask_user', function (request, response) {
+  var sender = request.body.sender;
+  switch (sender) {
+    case 'jack_brown*stellar-kumano-te.com':
+      response.status(403).end();
+      break;
+    case 'steintor_jakupsson*stellar-kumano-te.com':
+      console.log('need human help!');
+      response.status(202).json({pending: 3600}).end();
+      break;
+    default:
+      response.status(200).end();
+      break;
+  }
+});
 
 app.listen(8005, function () {
   console.log('Bridge server callbacks running on port 8005!');
---
+++ docker-compose-run-callbacks.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker-compose run -p 8005:8005 callbacks-kumano-te.com /bin/bash
