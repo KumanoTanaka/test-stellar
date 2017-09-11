90b63fdb7cc5d58b35c3f326dc5f3c2d7392f49c
KumanoTanaka <hiroki.tanaka@kumano-te.com>
2017-09-12 01:17:59 +0900

    refs #2

--- docker-compose-run-bridge.sh
+++ docker-compose-run-bridge.sh
@@ -2,4 +2,4 @@
 
 # ./wait-for-it.sh bridge-db:3306 -t 600 -s -- ./bridge --migrate-db
 
-docker-compose run -p 8001:8001 bridge /bin/bash
+docker-compose run -p 8001:8001 bridge-kumano-te.com /bin/bash
